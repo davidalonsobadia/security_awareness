cf44fd66716f242af8e749f49c404c07f5043560
David <david.alonso@eurecat.org>
2017-02-13 16:31:50 +0100

    Zones: findByActivitiesInterestedOrAssisted added

--- src/main/java/com/example/repository/ZoneRepository.java
+++ src/main/java/com/example/repository/ZoneRepository.java
@@ -1,10 +1,12 @@
 package com.example.repository;
 
+import java.util.Set;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import com.example.annotations.PreAuthorizeAdmin;
-import com.example.model.UserConfiguration;
 import com.example.model.Zone;
 
 public interface ZoneRepository extends CrudRepository<Zone, Long> {
@@ -22,4 +24,13 @@ public interface ZoneRepository extends CrudRepository<Zone, Long> {
 	void deleteAll();
 
 	Zone findByName(@Param("name") String name);
+	
+	@Query("SELECT z from Zone z, ActivityStatus actstat "
+			+ "JOIN actstat.user u "
+			+ "JOIN actstat.activity act "
+			+ "JOIN act.zone actzone "
+			+ "WHERE u.email LIKE :user "
+			+ "AND actzone.id = z.id "
+			+ "AND (actstat.interested = true OR actstat.assistant = true)")
+	Set<Zone> findByActivitiesInterestedOrAssisted(@Param("user") String user);
 }
