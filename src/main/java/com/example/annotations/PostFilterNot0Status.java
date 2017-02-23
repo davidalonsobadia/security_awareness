cd29bfa14bcb9560df3265c766233c9be6e6435c
David <david.alonso@eurecat.org>
2017-02-23 13:20:45 +0100

    same as previous commit

---
+++ src/main/java/com/example/annotations/PostFilterNot0Status.java
@@ -0,0 +1,19 @@
+package com.example.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.security.access.prepost.PostFilter;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@PostFilter("hasRole('ADMIN') or (filterObject.user.email == principal.username and filterObject.status != 0)")
+public @interface PostFilterNot0Status {
+
+}
