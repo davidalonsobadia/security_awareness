4504702f525ccfa8ba1f72e9f1c107dc735dbaa8
David <david.alonso@eurecat.org>
2017-03-10 09:58:36 +0100

    Bug fixed - ActivityBlock added to ActivityExpanded Projection.

--- src/main/java/org/security_awareness/model/projections/ActivitiesExpanded.java
+++ src/main/java/org/security_awareness/model/projections/ActivitiesExpanded.java
@@ -1,8 +1,10 @@
 package org.security_awareness.model.projections;
 
 import java.util.Date;
+import java.util.List;
 
 import org.security_awareness.model.Activity;
+import org.security_awareness.model.ActivityBlock;
 import org.security_awareness.model.User;
 import org.security_awareness.model.Zone;
 import org.springframework.data.rest.core.config.Projection;
@@ -17,6 +19,8 @@ public interface ActivitiesExpanded {
 	int getNumRepeats();
 	int getType();
 	String getDescription();
+	
+	List<ActivityBlock> getActivitiesBlock();
 	
 	Zone getZone();
 	User getManager();
